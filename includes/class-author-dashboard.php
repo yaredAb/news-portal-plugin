e83be9c0529381e9d700712f7f50d341d387d6f0
Yared <yaredyh11@gmail.com>
2025-01-24 11:50:32 +0300

    display list of aricles in author dashboard page

--- includes/class-author-dashboard.php
+++ includes/class-author-dashboard.php
@@ -18,8 +18,38 @@ class Author_Dashboard {
   
     }
 
+
     public function display_author_dashboard() {
-            echo '<h1>Author Dashboard</h1>';
+        echo '<div class="wrap">';
+        echo '<h1>Your Articles</h1>';
+        $this->list_author_articles();
+        echo '</div>';
+    }
+
+    public function list_author_articles() {
+        $user_id = get_current_user_id();
+        $args = [
+            'author'=>$user_id,
+            'post_type' => 'post',
+            'post_status' => ['draft', 'publish', 'pending', 'future'],
+        ];
+        $query = new WP_Query($args);
+        if($query->have_posts()) {
+            echo '<table class="widefat fixed">';
+            echo '<thead><tr><th>Title</th><th>Status</th><th>Date</th></tr></thead><tbody>';
+            while($query->have_posts()) {
+                $query->the_post();
+                echo '<tr>';
+                echo '<td>'.get_the_title().'</tr>';
+                echo '<td>'.get_post_status().'</tr>';
+                echo '<td>'.get_the_date().'</tr>';
+                echo '</tr>';
+            }
+            echo '</tbody></table>';
+        }else{
+            echo '<p>No articles</p>';
+        }
+        wp_reset_postdata();
     }
 }
 new Author_Dashboard();
